083943f942ba896516d8d5aedd787cf7a119e681
ApoGouv <apo.gouv@gmail.com>
2024-12-26 00:23:12 +0200

    First working data :D

--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,10 @@
 import os
 
 from extractors.extract_scrabble_word_refs import extract_scrabble_word_refs
-
+from utils.fix_scrabble_refs import fix_scrabble_refs
+from utils.filter_words import filter_scrabble_words
+from utils.merge_refs_with_scrabble_words import merge_refs_with_scrabble_words
+from utils.preprocess_scrabble_data import preprocess_scrabble_data
 
 def run_pipeline():
     """
@@ -36,7 +39,7 @@ def run_pipeline():
     # Configuration for Scrabble Word References (swr)
     swr_pdf_path = "assets/pdf/scrabble-word-refs-2020-02-12.pdf"
     swr_start_page = 5
-    swr_end_page = 6  # Maximum page number is 67
+    swr_end_page = 67  # Maximum page number is 67
 
     swr_output_txt_dir = f"{output_txt_dir}/scrabble_word_refs"
     # Create output directory if it doesn't exist
@@ -46,7 +49,26 @@ def run_pipeline():
     swr_output_json_file = f"{output_json_dir}/scrabble_words_with_refs.json"
 
     # Call the extraction function for Scrabble Words References
-    extract_scrabble_word_refs(swr_pdf_path, swr_start_page, swr_end_page, swr_output_txt_dir, swr_output_json_file)
+    # extract_scrabble_word_refs(swr_pdf_path, swr_start_page, swr_end_page, swr_output_txt_dir, swr_output_json_file)
+
+    swrfix_output_json_file = f"{output_json_dir}/scrabble_words_with_refs_fixed.json"
+    # fix_scrabble_refs(swr_output_json_file, swrfix_output_json_file)
+
+    swrfiltered_output_json_file = f"{output_json_dir}/scrabble_words_with_refs_2_to_8_chars.json"
+    # filter_scrabble_words(swrfix_output_json_file, swrfiltered_output_json_file)
+
+    merged_scrabble_words_output_json_file = f"{output_json_dir}/merged_scrabble_words.json"
+    # merge_refs_with_scrabble_words(
+    #     dict_file=sw_output_json_file,
+    #     fixed_file=swrfiltered_output_json_file,
+    #     output_file=merged_scrabble_words_output_json_file)
+    
+    final_output_words_file = f"{output_json_dir}/scrabble_words_2_to_8_chars.json"
+    final_output_stats_file = f"{output_json_dir}/scrabble_words_2_to_8_chars_metadata.json"
+    preprocess_scrabble_data(
+        input_file=merged_scrabble_words_output_json_file, 
+        output_words_file=final_output_words_file, 
+        output_stats_file=final_output_stats_file)
 
 if __name__ == "__main__":
     run_pipeline()
