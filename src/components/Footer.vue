fe9e90f5764749b9fadd48b6acdf29e28711303e
ApostolosG <apo.gouv@gmail.com>
2024-12-31 21:08:49 +0200

    Added powered by to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,21 @@
+<script setup lang="ts">
+  import LineMdCoffeeHalfEmptyFilledLoop from '@/icons/Coffee.vue'
+</script>
+
+
 <template>
   <footer class="bg-teal-600 text-white py-4 mt-8 shadow-inner">
     <div class="container mx-auto px-4 flex justify-between items-center">
       <!-- Copyright Info -->
       <p class="text-sm text-gray-200">
         &copy; 2024 Scrabble Word Finder. All rights reserved.
-        <span class="ml-2 italic">Made with coffee by ApoGouv</span>
+        <span class="powered ml-2 italic">
+          Made by&nbsp;
+          <a href="https://github.com/ApoGouv/" target="_blank" rel="noopener noreferrer" class="text-teal-200 hover:text-teal-300">
+            ApoGouv
+          </a>
+          , powered by <LineMdCoffeeHalfEmptyFilledLoop/>
+        </span>
       </p>
 
       <div class="flex space-x-4">
@@ -15,3 +26,16 @@
     </div>
   </footer>
 </template>
+
+<style scoped>
+.powered {
+  display: inline-flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+
+.powered svg {
+  display: inline-flex;
+  margin: 0 3px;
+}
+</style>
