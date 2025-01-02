3135f86bf79de3956a317625e41b07e9551c2467
ApostolosG <apostolosg@mentalmedia.gr>
2025-01-02 10:55:06 +0200

    added z-index to nav to stay atop of everything

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- ====== Navbar Section Start -->
-  <nav class="bg-teal-600 text-white shadow-md sticky top-0 w-full">
+  <nav class="bg-teal-600 text-white shadow-md sticky top-0 w-full z-50">
     <div class="container mx-auto px-4 py-3 flex items-center justify-between">
       <!-- App Title -->
       <a href="#" class="text-lg font-bold text-white tracking-wide hover:text-teal-200">
