af874da3c45280edc7005d0be8ecc028e15594b6
ApostolosG <apo.gouv@gmail.com>
2025-01-05 11:48:03 +0200

    updated import paths with alias

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
-import './style.css';
-import App from './App.vue';
+import '@/style.css';
+import App from '@/App.vue';
 import Toast, { POSITION } from 'vue-toastification';
 import type { PluginOptions } from 'vue-toastification';
 // Import the vue-toastification CSS
