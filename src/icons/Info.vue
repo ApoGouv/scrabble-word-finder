9ca4f00a4323c853abb5feef08389408a56a4748
ApostolosG <apostolosg@mentalmedia.gr>
2025-01-08 12:23:13 +0200

    Added Info Icon and changes header Help icon to Info icon

---
+++ src/icons/Info.vue
@@ -0,0 +1,41 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :class="customClass"
+    :style="customStyle"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    role="img"
+    focusable="false"
+    aria-label="Info Icon"
+  >
+    <title>Info</title>
+    <path
+      fill="currentColor"
+      d="M11 17h2v-6h-2zm1-8q.425 0 .713-.288T13 8t-.288-.712T12 7t-.712.288T11 8t.288.713T12 9m0 13q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22"
+    />
+  </svg>
+</template>
+
+<script lang="ts">
+  import type { PropType } from 'vue';
+
+  /**
+   * material-symbols:info
+   * @see https://icones.js.org/collection/all?s=info&icon=material-symbols:info
+   */
+  export default {
+    name: 'InfoIcon',
+    props: {
+      customClass: {
+        type: String,
+        default: '',
+      },
+      customStyle: {
+        type: [String, Object] as PropType<string | Record<string, any>>,
+        default: '',
+      },
+    },
+  };
+</script>
